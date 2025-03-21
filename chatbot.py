84944e023ab060f7425b4a35849db226859b229a
Bichu George <99749851+BichuGeorge@users.noreply.github.com>
2025-03-21 21:04:59 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,7 @@
+"""
+We will build a chatbot whic will have the following capabilities:
+1. will be able to have a conversation
+2. will remember previous interactions: will have memory
+3. will be able to have different memories for different user sessions
+4. will be able to remember a limited number of messages: limited memory
+"""
